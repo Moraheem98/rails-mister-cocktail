859bfd0c92fc5a5a6853a14b9b971a67ef9e0583
Mohammed Raheem <mojoraheem@icloud.com>
2020-08-13 17:50:26 +0100

    new methods added to cocktails controller

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,9 +1,27 @@
 class CocktailsController < ApplicationController
   def index
-    @cocktails = Cocktail.all
+    @cocktail = Cocktail.all
   end
 
   def show
-    @cocktails = Cocktail.find(params[:id])
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+     @cocktail = Cocktail.new(cocktail_params)
+     if @cocktail.save
+      redirect_to @cocktail
+     else
+      render
+  end
+
+  private
+
+  def cocktail_params
+    params.require(cocktail).permit(:name)
   end
 end
